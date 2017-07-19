3811c8326cbd53a8fa23c51e07f650f70afdd3fa
Jaga Santagostino <jagasantagostino@gmail.com>
2017-07-19 23:36:55 +0200

    initial commit

---
+++ exercises.el
@@ -0,0 +1,16 @@
+(message "ciaone")
+(insert "ciaone")
+(quote(1 2 3))
+'(1 2 3)
+
+(car '(1 2 3))
+(cdr '(1 2 3))
+ '()
+ ()
+ nil
+
+(null ())
+
+(cons 1 '(1 2))
+(cons 1 2)
+(cons (cons 1 '(2)) '(2 3))
