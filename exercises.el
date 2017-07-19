065e57b9d86c1ee2a117136a984fe6500f18af06
Jaga Santagostino <jagasantagostino@gmail.com>
2017-07-20 00:37:24 +0200

    add more comments

--- exercises.el
+++ exercises.el
@@ -1,16 +1,17 @@
-(message "ciaone")
-(insert "ciaone")
-(quote(1 2 3))
-'(1 2 3)
+(message "ciaone") ; pronts ciaone in minibuffer
+(insert "ciaone") ; inserts ciaone at cursor
+(quote(1 2 3)) ; (1 2 3)
+'(1 2 3) ; (1 2 3)
+(car '(1 2 3)) ; 1
+(cdr '(1 2 3)) ; ( 2 3)
+ '() ; nil
+ () ; nil
+ nil ; nil
 
-(car '(1 2 3))
-(cdr '(1 2 3))
- '()
- ()
- nil
+(null ()) ; t
 
-(null ())
+(cons 1 '(1 2)) ; (1 1 2)
+(cons 1 2) ; (1 . 2)
+(cons (cons 1 '(2)) '(3 4)) ; ((1 2) 3 4)
 
-(cons 1 '(1 2))
-(cons 1 2)
-(cons (cons 1 '(2)) '(2 3))
+(append '(1 2) '(3 4)) ; (1 2 3 4)
